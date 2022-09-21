d829b8b1b77a7ed9b785fe21cc440bc3a61f3176
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-09-21 23:06:36 +0200

    [feat: tutorial-details.component.ts] implemented getTutorial method

--- src/app/components/tutorial-details/tutorial-details.component.ts
+++ src/app/components/tutorial-details/tutorial-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {TutorialService} from "src/app/services/tutorial.service";
+import {ActivatedRoute,Router} from "@angular/router";
 
 @Component({
   selector: 'app-tutorial-details',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TutorialDetailsComponent implements OnInit {
 
-  constructor() { }
+  currentTutorial:any = null;
+  message = '';
+
+  constructor(private tutorialService:TutorialService,
+    private route:ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    this.message = '';
+    this.getTutorial(this.route.snapshot.paramMap.get('id'));
+  }
+
+  getTutorial(id:any){
+        this.tutorialService.get(id).subscribe(data=>{
+          this.currentTutorial = data;
+          console.log(data);
+        }, error=>{
+          console.log(error);
+        })
   }
 
 }
